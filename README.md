d70c72e6d34e9e4c5b28c87b49d4e9d7b8f52075
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-04-09 19:55:32 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # android-tabs-to-bookmarkshtml
-node application for converting android chrome tabs into bookmarks html file.
+Node application for converting android chrome tabs into bookmarks html file.
+
+## Instructions
+
+1. Create a source.txt file in the same folder as converter.js
+2. Follow instructions here: https://developers.google.com/web/tools/chrome-devtools/remote-debugging/
+* Expand the list of tabs to get the full list
+* Highlight and copy everything that's shown
+* Paste into source.txt
+3. Run converter.js using node: `node converter.js`
+4. Output is a bookmarks html file called `androidTabsBookmarks[curUnixTime].html`
+5. This file is ready to import into the browser of your choice
+
---
+++ converter.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+const source = require('./source.txt');
+
+let parsed = source.split("\r\n");
+//take out extra lines from source
+parsed = parsed.filter(x => x !== "")
+        .filter(x => x !== "Inspect")
+        .filter(x => x !== "View less tabs…");
+
+//create the beginning of the template
+let template = `<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file.
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p>
+        <DT><H3 ADD_DATE="${Date.now()}" LAST_MODIFIED="${Date.now()}">Android Bookmarks Folder</H3>
+        <DL><p>\n`;
+
+//populate the template       
+for(var i = 0; i < parsed.length; i++) {
+    if(!parsed[i].includes("http")) {
+        template += `<DT><A HREF="${parsed[i+1]}" ADD_DATE="${Date.now()}" ICON=" ">${parsed[i]}</A>\n`} 
+        
+};
+// close the template
+template += `</DL>
+    </DL>
+    `;
+//name the file
+let bookmarksFilename = 'androidTabsBookmarks'+ Date.now() + '.html'
+// create the file
+fs.appendFile(bookmarksFilename, template, 'utf8', (err) => {
+    if (err) {console.log(err)};
+    console.log(`Created ${bookmarksFilename}`);
+});
+
+
